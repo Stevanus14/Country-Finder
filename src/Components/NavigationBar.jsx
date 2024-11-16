270d5926d588f19fbe86a374216e5bec1d7b275b
steva <stevanuskristianto140801@gmail.com>
2024-11-16 18:15:10 +0700

    fix navbar

--- src/Components/NavigationBar.jsx
+++ src/Components/NavigationBar.jsx
@@ -5,19 +5,18 @@ const NavigationBar = () => {
   const navigate = useNavigate();
   return (
     <div>
-      <Navbar id="nav">
+      <Navbar id="nav" expand="lg" bg="light" variant="light">
         <Container>
-          <Navbar.Brand>
+          <Navbar.Brand onClick={() => navigate("/")}>
             <b>Country Finder</b>
           </Navbar.Brand>
-          <Nav>
-            <Nav.Link onClick={() => navigate("/")} className="nav-link">
-              Countries
-            </Nav.Link>
-            <Nav.Link onClick={() => navigate("/cooperation")} className="nav-link">
-              Cooperation
-            </Nav.Link>
-          </Nav>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link onClick={() => navigate("/")}>Countries</Nav.Link>
+              <Nav.Link onClick={() => navigate("/cooperation")}>Cooperation</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
     </div>
